cdf9b13de318dc38a53e5058a43e6d0d22eebfee
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-10-07 16:32:00 +0700

    update promt

--- chat.py
+++ chat.py
@@ -3,28 +3,23 @@ import os
 import langchain
 import numpy as np
 import openai
+from dotenv import load_dotenv
 from langchain import hub
-from langchain.agents import (
-    AgentExecutor,
-    Tool,
-    create_openai_tools_agent,
-    create_react_agent,
-    tool,
-)
-from langchain.chains import LLMMathChain, RetrievalQA
+from langchain.agents import AgentExecutor, Tool, create_react_agent
+from langchain.chains.llm_math.base import LLMMathChain
 from langchain.memory import ConversationEntityMemory
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage import LocalFileStore
 from langchain.storage._lc_store import create_kv_docstore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.tools.retriever import create_retriever_tool
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts.prompt import PromptTemplate
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
-from pydantic import BaseModel, Field
+
+load_dotenv(verbose=True, override=True)
 
 OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
 OPENAI_DEPLOYMENT_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
@@ -94,7 +89,7 @@ doc_prompt = PromptTemplate.from_template(
 tool_search = create_retriever_tool(
     retriever=retriever,
     name="Search BonBon",
-    description="Searches and returns answer from BONBON FAQ.",
+    description="Searches and returns answer from BONBON FAQ. use when promt contains words: internet connection, printer, malware issues, ...",
     document_prompt=doc_prompt,
 )
 
@@ -106,7 +101,7 @@ tools = [
     Tool(
         name="Duck Duck Go",
         func=duckduck.run,
-        description="useful for when you need to search for more information on the internet with Duck Duck Go search",
+        description="useful for when you need to search for more information on the internet with Duck Duck Go search, but use it only once",
     ),
     Tool(
         name="Calculator",
@@ -116,28 +111,27 @@ tools = [
 ]
 
 template = """
-Answer the following questions as best you can. You can use history {history} to fill in unknown context. You have access to the following tools:
+Answer the following questions as best you can, but don't repeat your thoughts or actions, if you see repeat thoughts or actions, return the answer immediately. You can use history {history} to fill in unknown context. You have access to the following tools:
 
 {tools}
-
-Use the following format:
-Remember: If any invalid format occurs, terminate and return answer
+If you encounter any error, provide the answer immediately
+If you reach max iteration, provide the answer immediately
+Print and Use the following format:
 Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, find in Search BonBon tool first, then should be one of [{tool_names}]. If not, terminate answer
-Action Input: the input to the action
+Thought: you should always think about what to do and follow with Action, do not repeat the thought but return the previous answer from {history} and do not use any tool if it is not needed. 
+Action: the action to take, find in Search BonBon tool first, then use other tools. If tool is None, use Duck Duck Go tool", then MUST be one of [{tool_names}]. 
+Action Input: the input to the action. Do NOT repeat the action input in your {history}
 Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat at most 5 times.)
-Thought: I now know the final answer. Let's return the answer
+... (this Thought/Action/Action Input/Observation can repeat N times.)
+Thought: I now know the final answer
+Action: Return the final answer
 Final Answer: the final answer to the original input question. If use Search BonBon then include the page of the PDF file that has the question
 
-Begin!
-Context:
-{entities}
-
 Current conversation:
-Chat History:
-{history}
+Chat History: {history}
+
+Begin!
+Context: {entities}
 Last line:
 Human: {input}
 Thought: {agent_scratchpad}
@@ -151,12 +145,12 @@ react_agent_executor = AgentExecutor(
     verbose=True,
     handle_parsing_errors=True,
     memory=ConversationEntityMemory(llm=llm, top_k=3),
-    max_iterations=3
+    max_iterations=5,
 )
 i = input("enter promt ('exit' to terminate): ")
 while i.lower() != "exit":
     query = i
-    print("Responding ...")
+    print("Responding to " + i)
     response = react_agent_executor.invoke({"input": query})
     result = response.get("output")
     print(result)
